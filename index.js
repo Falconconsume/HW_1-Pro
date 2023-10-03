0045c9307ea9a47ec1143cfd268ed6518ccb114e
Simply Done <olko4455@gmail.com>
2023-10-03 16:12:58 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("Hello!");
+
+var user_name = prompt("What is your name");
+
+let country = prompt("Where are you from?");
+
+alert(`Nice to meet you, ${user_name}! You are from ${country}!`);
